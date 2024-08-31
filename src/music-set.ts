0c192f809a10f2c507cf396226163779d86b1df0
takato <taka.rio.shio.2012.4.26@gmail.com>
2024-08-31 14:06:09 +0900

    曲の追加とjsの修正変更

--- src/music-set.ts
+++ src/music-set.ts
@@ -6,13 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
   const audioPlayer = document.getElementById('audio-player')!;
   const selectButton = document.getElementById('select-button') as HTMLButtonElement;
 
+  if (!audioPlayer) {
+    console.error('audio-player 要素が見つかりません。');
+    return;
+  }
+
   songItems.forEach((item) => {
     item.addEventListener('click', function () {
+      //選択された楽曲を再生
       selectedSong = item.getAttribute('data-song');
       const songTitle = item.getAttribute('data-title');
-
+      //
       selectedSongElement!.textContent = songTitle;
       (audioPlayer.firstElementChild as HTMLSourceElement).src = String(selectedSong);
+      console.log('Selected song', selectedSong);
       selectButton.disabled = false;
 
       // 他の選択項目から選択状態を解除
